25a2d392c9f2486723a43f9e6b9a41cec5766ca1
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-29 10:22:40 +0500

    lpnb

---
+++ integer3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace integer3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Byt = int.Parse(Console.ReadLine());
+            var result = Byt / 1024;
+            Console.WriteLine(result+" ");
+        }
+    }
+}
